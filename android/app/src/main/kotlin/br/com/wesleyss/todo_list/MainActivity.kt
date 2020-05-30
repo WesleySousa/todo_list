66434f523e016ef5844fba498048667d801d44d2
Wesley Silva <wesley@simplecodeti.com.br>
2020-05-29 22:41:38 -0300

    create functions of manipulation data of the todo

---
+++ android/app/src/main/kotlin/br/com/wesleyss/todo_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.wesleyss.todo_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:convert';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List _todoList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return new File('${directory.path}/data.json');
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_todoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (err) {
+      return null;
+    }
+  }
+}
